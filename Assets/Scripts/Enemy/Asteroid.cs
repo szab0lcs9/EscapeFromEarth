a7f85a37a8e56550b9f7341e608913908689220b
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2024-02-13 21:05:17 +0100

    made Asteroid poolable and added movement

--- Assets/Scripts/Enemy/Asteroid.cs
+++ Assets/Scripts/Enemy/Asteroid.cs
@@ -4,17 +4,28 @@ using UnityEngine.Pool;
 public class Asteroid : MonoBehaviour, IEnemy, IDamageable
 {
     ObjectPool<Asteroid> pool;
+    const float minForce = -10f;
+    const float maxForce = 10f;
 
     [SerializeField]
-    private float health = 100f;
+    private float intitalVelocity;
+
+    [SerializeField]
+    private float health;
     public float Health { get => health; set => health = value; }
 
     private float shield;
     public float Shield { get => shield; set => shield = 0; }
 
-    public void SetPool(ObjectPool<Asteroid> pool)
+    void Awake()
+    {
+        MoveAsteroid();
+    }
+
+    public void Initialize(ObjectPool<Asteroid> pool, float health)
     {
         this.pool = pool;
+        this.health = health;
     }
 
     public void Die()
@@ -32,4 +43,10 @@ public class Asteroid : MonoBehaviour, IEnemy, IDamageable
     }
 
     public void Attack() { }
+
+
+    public void MoveAsteroid()
+    {
+        gameObject.GetComponent<Rigidbody>().AddRelativeForce(Random.Range(minForce, maxForce), 0.0f, Random.Range(minForce, maxForce));
+    }
 }
