1c5ab5b1f24904d6d63d11694b99d0fc40d6a311
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2024-02-24 10:54:55 +0100

    move the IEnemy's Attack method to IAttackable interface

--- Assets/Scripts/Interfaces/IAttackable.cs
+++ Assets/Scripts/Interfaces/IAttackable.cs
@@ -1,9 +1,8 @@
-using Assets.Scripts.Enemy;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IEnemy
+public interface IAttackable
 {
-
+    void Attack();
 }
