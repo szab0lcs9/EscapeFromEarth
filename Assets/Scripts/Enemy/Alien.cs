0b15495c6c5309534bb7e407542b4693005423e5
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2024-02-25 21:37:27 +0100

    implemented methods

--- Assets/Scripts/Enemy/Alien.cs
+++ Assets/Scripts/Enemy/Alien.cs
@@ -1,34 +1,40 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Pool;
 
-public class Alien : MonoBehaviour, IEnemy, IDamageable
+public class Alien : MonoBehaviour, IEnemy, IDamageable, IAttackable
 {
     const float HEALTH_DAMAGE_RATIO_WHEN_HAS_SHIELD = 0.1f;
 
-    ObjectPool<Alien> pool;
+    ObjectPool<Alien> alienPool;
+    Rigidbody rb;
     new Transform transform;
-
-    [SerializeField]
-    private float health;
-    public float Health { get => health; set => health = value; }
-
-    [SerializeField]
-    private float shield;
-    public float Shield { get => shield; set => shield = value; }
+    Missile missile;
 
     bool hasShield;
 
+    [SerializeField] GameObject missilePrefab;
+    [SerializeField] Transform missileSpawnPoint;
+    [SerializeField] float asteroidDetectionRange;
+    [SerializeField] float avoidanceForce;
+
+    [SerializeField] private float health;
+    public float Health { get => health; set => health = value; }
+
+    [SerializeField] private float shield;
+
+    public float Shield { get => shield; set => shield = value; }
+
     public void Attack()
     {
-
+        GameObject _missile = Instantiate(missilePrefab, missileSpawnPoint.position, missileSpawnPoint.rotation);
+        missile = _missile.GetComponent<Missile>();
+        missile.Launch();
     }
 
     public void Die()
     {
-        pool.Release(this);
+        alienPool.Release(this);
     }
 
     public void TakeDamage(float damageTaken)
@@ -45,11 +51,13 @@ public class Alien : MonoBehaviour, IEnemy, IDamageable
             Die();
     }
 
-    public void Initialize(ObjectPool<Alien> pool, float health, float shield)
+    public void Initialize(ObjectPool<Alien> pool, float health, float shield, Vector3 position)
     {
-        this.pool = pool;
+        this.alienPool = pool;
         this.health = health;
         this.shield = shield;
+        transform = GetComponent<Transform>();
+        transform.position = position;
     }
 
     void OnEnable()
@@ -59,22 +67,39 @@ public class Alien : MonoBehaviour, IEnemy, IDamageable
 
     void Start()
     {
-        transform = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
+        gameObject.transform.position.Set(transform.position.x, 0.0f, transform.position.z);
     }
 
-    public void StopMovement()
+    internal void StopMovement()
     {
-
+        if (gameObject.GetComponent<Rigidbody>().velocity.magnitude > 0.0f)
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
     }
 
     internal void AvoidFromAsteroids()
     {
+        Collider[] asteroids = Physics.OverlapSphere(transform.position, asteroidDetectionRange, LayerMask.GetMask("Asteroid"));
 
+        if (asteroids.Length > 0)
+        {
+            Vector3 avoidanceVector = Vector3.zero;
+            foreach (Collider asteroid in asteroids)
+            {
+                Vector3 avoidanceDirection = transform.position - asteroid.transform.position;
+                avoidanceVector += avoidanceDirection.normalized / avoidanceDirection.magnitude;
+                avoidanceVector.y = 0;
+            }
+            rb.AddForce(avoidanceVector * avoidanceForce, ForceMode.Impulse);
+        }
     }
+    internal void MoveTowardsPlayer(Transform player)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, 0.001f);
+    }
+
 }
