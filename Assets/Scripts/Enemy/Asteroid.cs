1b639a3725641236a4b970ec1980cd6be5148ad9
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2024-03-12 21:18:36 +0100

    made asteroid explodeable

--- Assets/Scripts/Enemy/Asteroid.cs
+++ Assets/Scripts/Enemy/Asteroid.cs
@@ -1,24 +1,27 @@
 using UnityEngine;
 using UnityEngine.Pool;
 
-public class Asteroid : MonoBehaviour, IEnemy, IDamageable
+public class Asteroid : MonoBehaviour, IEnemy, IDamageable, IExplodable
 {
+
     ObjectPool<Asteroid> pool;
+    CollectibleFactory collectibleFactory;
     const float minForce = -10f;
     const float maxForce = 10f;
 
-    [SerializeField]
-    private float intitalVelocity;
+    [SerializeField] GameObject explosionParticle;
+    [SerializeField] GameObject collectiblePrefab;
+    [SerializeField] float intitalVelocity;
 
-    [SerializeField]
-    private float health;
+    [SerializeField] float health;
     public float Health { get => health; set => health = value; }
 
-    private float shield;
+    float shield;
     public float Shield { get => shield; set => shield = 0; }
 
     void Awake()
     {
+        collectibleFactory = new CollectibleFactory();
         MoveAsteroid();
     }
 
@@ -30,6 +33,7 @@ public class Asteroid : MonoBehaviour, IEnemy, IDamageable
 
     public void Die()
     {
+        collectibleFactory.SpawnCollectible(collectiblePrefab, transform.position);
         pool.Release(this);
     }
 
@@ -45,5 +49,11 @@ public class Asteroid : MonoBehaviour, IEnemy, IDamageable
     public void MoveAsteroid()
     {
         gameObject.GetComponent<Rigidbody>().AddRelativeForce(Random.Range(minForce, maxForce), 0.0f, Random.Range(minForce, maxForce));
+    }
+
+    public void Explode()
+    {
+        GameObject _explosionParticle = Instantiate(explosionParticle, gameObject.transform.position, Quaternion.identity);
+        Destroy(_explosionParticle, 2f);
     }
 }
